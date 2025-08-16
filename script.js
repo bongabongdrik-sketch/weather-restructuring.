3b91f947a182c6b316c5b92d86e4148ab34ed35e
bongabongdrik-sketch <bongabongdrik@gmail.com>
2025-08-16 09:21:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const fakeData = { 
+  location: { name: "Manila" }, 
+  current: { 
+    temp_c: 30, 
+    condition: { 
+      text: "Partly Cloudy", 
+    }, 
+  }, 
+};
+
+// Destructure the data
+const {
+  location: { name },
+  current: {
+    temp_c,
+    condition: { text },
+  },
+} = fakeData;
+
+// Output to console
+console.log(`Weather in ${name}: ${temp_c}°C and ${text}`);
 
